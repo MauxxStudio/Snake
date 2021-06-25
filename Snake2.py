692746bc66e08c340b4cb1a981b1deedcb576cbf
Mauxx <mchamorro.ing@gmail.com>
2021-06-24 21:47:02 -0300

    Agregué: Pausa, Puntaje y Tiempo

--- Snake2.py
+++ Snake2.py
@@ -1,27 +1,35 @@
 
-import pygame, sys, time, random
+import pygame, sys, random
+
+from pygame.event import clear
 
 pygame.init()
 
 grid = 15,15
 dim = 20
+banner = 50
 
-size = width, height = dim*grid[0], dim*grid[1]
+width, height = dim*grid[0], dim*grid[1]
+size = width, height + banner
+
 screen = pygame.display.set_mode(size)
 
 backColor = (0,180,0)
+bannerColor = (32,0,32)
 snakeColor = (32,96,0)
 foodColor = (128,32,32)
+fontColor = (255,200,200)
 
-snake = [ (int(grid[0]/2)-1,int(grid[1]/2)),(int(grid[0]/2),int(grid[1]/2)),(int(grid[0]/2)+1,int(grid[1]/2)) ]
-snakehead = snake[2]
+font = pygame.font.Font(pygame.font.get_default_font(), 30)
+
 directions = [(1,0),(0,1),(-1,0),(0,-1)]
 dir = 0
-food = ()
 
 game = True
-play = True
+play = False
 pause = True
+init = True
+score = 0
 
 while game:
     for event in pygame.event.get():
@@ -33,18 +41,26 @@ while game:
                     dir = (dir - 1) % 4
                 elif event.key == pygame.K_RIGHT:
                     dir = (dir + 1) % 4
-                else: pause = not pause
+                elif event.key == pygame.K_SPACE:
+                    pause = not pause
             else:
                 if event.key == pygame.K_ESCAPE:
                     game = False
-                else: play = True
+                else:
+                    play = True
 
     if play == False:
 
         snake = [ (int(grid[0]/2)-1,int(grid[1]/2)),(int(grid[0]/2),int(grid[1]/2)),(int(grid[0]/2)+1,int(grid[1]/2)) ]
         snakehead = snake[2]
+        food = ()
+        score = 0
+        time_0 = pygame.time.get_ticks()
+        if init:
+            init = False
+            play = True
 
-    else:
+    elif pause:
         snakehead = ((snakehead[0]+directions[dir][0]) % grid[0],(snakehead[1]+directions[dir][1]) % grid[1])
 
         if snake.count(snakehead):
@@ -52,6 +68,9 @@ while game:
         else:
             snake.append(snakehead)
             if snakehead == food:
+                score = score + 5
+                
+                
                 food = ()
             else:
                 snake.pop(0)
@@ -60,15 +79,21 @@ while game:
             food=(random.randint(0,grid[0]-1),random.randint(0,grid[1]-1))
             if snake.count(food): food = ()
 
-        time.sleep(0.1)
-
         screen.fill(backColor)
         
-        pygame.draw.rect(screen,foodColor,(food[0]*dim,food[1]*dim,dim,dim),width = 3)
+        pygame.draw.rect(screen,bannerColor,(0,0,width,banner),width = 0)
+        text = pygame.font.Font.render(font, str(((pygame.time.get_ticks()-time_0)//100)/10) ,True,fontColor)
+        screen.blit(text,(30,13))
+        text = pygame.font.Font.render(font,str(score),True,fontColor)
+        screen.blit(text,(width-70,13))
+        pygame.draw.rect(screen,foodColor,(food[0]*dim,food[1]*dim+banner,dim,dim),width = 0)
+
         for cell in snake:
-            pygame.draw.rect(screen,snakeColor,(cell[0]*dim,cell[1]*dim,dim,dim),width = 0)
+            pygame.draw.rect(screen,snakeColor,(cell[0]*dim,cell[1]*dim+banner,dim,dim),width = 0)
         
         pygame.display.flip()
+
+        pygame.time.wait(120)
 
 
 pygame.quit()
